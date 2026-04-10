5c444dbd33975fd9798e59db036c37e4bee59d40
Israa <israa.almahmoud23@gmail.com>
2026-04-10 13:48:28 +0200

    Refactor routes to remove commented code and streamline view handling

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,12 +6,9 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 
-// CORS preflight
 $routes->options('(:any)', static function () {
     return;
 });
 
-// frontend entry
 $routes->get('/', 'View::index');
 $routes->get('view/(:any)', 'View::index');
-$routes->get('(:any)', 'View::index');
